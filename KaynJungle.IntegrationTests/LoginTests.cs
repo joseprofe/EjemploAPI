c9729172ddc6b66580621fc4571d6447b52c6ce2
Jose <Jose@DESKTOP-I47R5VA>
2021-01-24 14:26:12 +0100

    Subiendo test project.

---
+++ KaynJungle.IntegrationTests/LoginTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KaynJungle.IntegrationTests
+{
+    public class LoginTests : IClassFixture<TestFixture<Startup>>
+    {
+        private HttpClient Client;
+
+        public LoginTests(TestFixture<Startup> fixture)
+        {
+            Client = fixture.Client;
+        }
+
+        [Fact]
+        public async Task LoginTests_LoginCorrect_ReturnsTrue()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/login",
+                Body = new
+                {
+                    Username = "jose",
+                    Password = "lol"
+                }
+            };
+
+            // Act
+            var response = await Client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            var value = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.True(value == "true");
+        }
+
+        [Fact]
+        public async Task LoginTests_LoginIncorrect_ReturnsFalse()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/login",
+                Body = new
+                {
+                    Username = "jose",
+                    Password = "lolete"
+                }
+            };
+
+            // Act
+            var response = await Client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            var value = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.True(value == "false");
+        }
+
+    }
+}
